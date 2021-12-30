f1c6e826752256d50142d3449e3c1326de4cccfa
ohadhell <81020823+ohadhell@users.noreply.github.com>
2021-12-30 18:33:24 +0200

    FINISHED

--- project.py
+++ project.py
@@ -8,13 +8,13 @@ import pandas as pd
 
 ##########constants########### 
 dataSet='marketing_campaign.csv'
-window_size = 400
-window_step = 200
+window_size = 200
+window_step = 80
 col_Not_Used=["ID","Dt_Customer","Z_CostContact","Z_Revenue","Complain"
 ,"AcceptedCmp4","AcceptedCmp5","AcceptedCmp1","AcceptedCmp2","AcceptedCmp3","Recency","MntWines"]
 
 col_list = ["Kidhome","Income","Year_Birth"
-,"Education","Marital_Status","Response","Teenhome"]#,	"MntFruits","MntMeatProducts",	"MntFishProducts"]
+,"Education","Marital_Status","Response"]#,"Teenhome",	"MntFruits","MntMeatProducts",	"MntFishProducts"]
 #,"MntSweetProducts",	"MntGoldProds",	"NumDealsPurchases",	"NumWebPurchases",	"NumCatalogPurchases",
 #"NumStorePurchases"	,"NumWebVisitsMonth"]
 ##########constants########### 
@@ -30,12 +30,11 @@ while(still_sliding):
       print("NUM RULES: ",len(rules))
       for rule in sorted(rules, key=lambda rule: rule.lift):
             print(rule)  # Prints the rule and its confidence, support, lift, ...
-      #print(rules)
       root = sp.createTree([theWindow])
-      #root2 = sp.createTree([theSecondWindow])
+      root2 = sp.createTree([theSecondWindow])
       loss1 = sp.suppress([root],[theWindow])
-      #loss2 = sp.suppressMin([root2],[theSecondWindow])
-      #print("loss 1: ",loss1,"  loss 2 : ",loss2)
+      loss2 = sp.suppressMin([root2],[theSecondWindow])
+      print("loss 1: ",loss1,"  loss 2 : ",loss2)
       print("AFTER SUPPRESSION: ")
       transactions = Window.getTransactions(theWindow)
       itemsets, rules = apriori(transactions, min_support=0.1, min_confidence=0.5,output_transaction_ids=True)
@@ -43,10 +42,8 @@ while(still_sliding):
       print("NUM RULES: ",len(rules_fil))
       for rule in sorted(rules_fil, key=lambda rule: rule.lift):
             print(rule)  # Prints the rule and its confidence, support, lift, ...
-      #print(rules_fil)
       still_sliding = Window.slide_window(theWindow)
-      #Window.slide_window(theSecondWindow)
-      still_sliding = False
+      Window.slide_window(theSecondWindow)
 
 
 
